1368e198d838419cb572fda1eec2a90e3fecc9da
Jonatasphp <62606709+Jonatasphp@users.noreply.github.com>
2020-08-06 21:01:08 -0300

    codificação do css e inserção do nivo slider

---
+++ README.md
@@ -0,0 +1,5 @@
+# Para iniciar o projeto basta rodar o comando abaixo:
+
+$ npm start
+
+## O projeto irá iniciar neste endereço http://127.0.0.1:8080/
---
+++ assets/js/scripts.js
@@ -0,0 +1,12 @@
+/*
+ * Configurando slider ao iniciar a página 
+ */
+function StartSlider() {
+    $('#slider').nivoSlider({
+        controlNav: false,
+        pauseTime: 2000, //ms
+        directionNav: false,
+
+    });
+}
+StartSlider();
